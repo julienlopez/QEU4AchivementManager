eadba286e3cfed138f3de2c54fdf3521d7387c1e
julienlopez <julien.lopez51@gmail.com>
2020-02-09 11:53:31 +0100

    writing json achivement file #1

--- src/datamanager.cpp
+++ src/datamanager.cpp
@@ -6,11 +6,32 @@
 #include <QDir>
 #include <QFileInfo>
 #include <QGuiApplication>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
 #include <QUrl>
 
 #include <QDebug>
 
 using std::placeholders::_1;
+
+namespace
+{
+QJsonObject toJson(const Achivemevent& achivement)
+{
+    QJsonObject res;
+    res["description"] = achivement.description;
+    res["title"] = achivement.title;
+    res["image"] = achivement.image_url;
+    return res;
+}
+
+void printError(const QString& error)
+{
+    qDebug() << "error : " << error;
+}
+
+} // namespace
 
 DataManager::DataManager(QNetworkAccessManager* network_manager, QObject* parent)
     : QObject(parent)
@@ -69,7 +90,7 @@ void DataManager::parseAchievementHtml(Result<QByteArray> html)
     AchivementHtmlParser parser{dataFolder().absolutePath()};
     html.and_then([&parser](QByteArray html) { return parser.parse(html); })
         .map(std::bind(&DataManager::saveAchivements, this, _1))
-        .or_else([](const QString& error) { qDebug() << "error: " << error; });
+        .or_else(&printError);
     setIsWorking(false);
 }
 
@@ -88,13 +109,15 @@ void DataManager::downloadImages()
         const auto name = url.right(url.size() - url.lastIndexOf('/') - 1);
         m_file_downloader->downloadFile(
             url, [n = QDir(dataImagesFolder().filePath()).filePath(name)](const Result<QByteArray> data) {
-                data.map(std::bind(&FileDownloader::writeFile, n, _1)).or_else([](const auto& error) {
-                    qDebug() << error;
-                });
+                data.map(std::bind(&FileDownloader::writeFile, n, _1)).or_else(&printError);
             });
     }
 }
 
 void DataManager::writeJsonFile()
 {
+    QJsonArray arr;
+    for(const auto& achivemevent : m_achivements)
+        arr << toJson(achivemevent);
+    FileDownloader::writeFile(dataFile().absoluteFilePath(), QJsonDocument{arr}.toJson());
 }
